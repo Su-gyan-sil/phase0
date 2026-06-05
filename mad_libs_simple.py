58c22e508c3c3fd9906abaa561b8d59387e705d0
Sugyansil Das <sugyansil.2006@gmail.com>
2026-06-06 02:37:10 +0530

    Building sentence

---
+++ mad_libs_simple.py
@@ -0,0 +1,4 @@
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter an adjective: ")
+print("The " + adjective + " " + noun + " " + verb + "s over the lazy dog.")
